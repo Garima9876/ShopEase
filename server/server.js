c43ff4da48a02ddae37861343c60b11999da8375
Garima9876 <garimabhatt987@gmail.com>
2024-09-02 13:17:51 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose
+  .connect(
+    "mongodb+srv://garimabhatt987:garimabhatt987654321@shopease-db.ejt2n.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials : true
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
