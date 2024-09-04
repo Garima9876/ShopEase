c3be9d9ed8bb0263980e28ee674bd2df135a0a77
Garima9876 <garimabhatt987@gmail.com>
2024-09-04 12:45:54 +0530

    did the basic redux setup in the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
-)
+);
---
+++ client/src/store/auth-slice/index.js
@@ -0,0 +1,20 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+const initialState ={
+    isAuthenticated : false,
+    isLoading : false,
+    user : null,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers : {
+        setUser : (state, action) => {
+
+        }
+    }
+})
+
+export const {setUser} = authSlice.actions;
+export default authSlice.reducer;
