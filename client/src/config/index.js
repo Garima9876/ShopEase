518d39d5cfd242b1f948bd7487a10c6df3b71ecd
Garima9876 <garimabhatt987@gmail.com>
2024-09-05 17:32:27 +0530

    created a common form component for whole application

---
+++ client/src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+    {
+        name: 'userName',
+        label: 'User Name',
+        placeholder: 'Enter you user Name',
+        componentType: 'input',
+        type: 'text',
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Enter you email',
+        componentType: 'input',
+        type: 'email',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Enter you password',
+        componentType: 'input',
+        type: 'password',
+    }
+]
